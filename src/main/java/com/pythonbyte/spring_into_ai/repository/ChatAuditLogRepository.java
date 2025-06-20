a9aeb5b8d4d72eae042bb735c21e98da834b30a0
Mark Nenadov <marknenadov@gmail.com>
2025-06-20 06:36:51 -0400

    clean up some junk

--- src/main/java/com/pythonbyte/spring_into_ai/repository/ChatAuditLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pythonbyte.spring_into_ai.repository;
-
-import com.pythonbyte.spring_into_ai.entity.ChatAuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatAuditLogRepository extends JpaRepository<ChatAuditLog, Long> {
-} 
